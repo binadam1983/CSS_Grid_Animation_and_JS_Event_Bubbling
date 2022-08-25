d31418895c5ebeb878727cb48e082ac552b743a2
BinAdam1983 <messagenaeem@skiff.com>
2022-08-26 01:39:37 +0500

    second commit - added some markup, styling and js

---
+++ main.js
@@ -0,0 +1,16 @@
+document.querySelector('.js-button').addEventListener('click', () => {
+    console.log('test')
+    document.querySelector('.js-grid').classList.toggle('grid--full')
+  })
+
+
+
+document.querySelector('.js-grid').addEventListener('click', (e) => {
+    console.log(e.target)
+    document.querySelector('.js-grid').classList.toggle('grid--full')
+})
+
+/* document.querySelector('.js-grid').addEventListener('mouseleave', () => {
+    console.log('item1 hovered')
+    document.querySelector('.js-grid').classList.remove ('grid--full')
+}) */
