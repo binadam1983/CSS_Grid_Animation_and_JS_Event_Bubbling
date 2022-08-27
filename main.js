c5884de07a311dbae6d11f5f11cd41e62f4c3698
BinAdam1983 <messagenaeem@skiff.com>
2022-08-27 14:35:14 +0500

    added comments to finalize

--- main.js
+++ main.js
@@ -3,26 +3,35 @@
     document.querySelector('.js-grid').classList.toggle('grid--full')
   }) */
 
+//clearing the state; starting anew
     window.localStorage.clear()
 
+//catching the event at the parent
     document.querySelector('.js-grid').addEventListener('click', (e) => {
     
     const target = e.target
 
+//calculating the clicked item's location using row and column
     
     let elementNumber = parseInt(target.classList[1])
     let selectRow = Math.floor(elementNumber / 4)    
     let selectColumn = elementNumber%4
 
+//adjusting the row for the 4th column
+
     if (selectColumn == 0){
         selectRow = selectRow - 1;
     }
+
+//saving & retrieving the state of clicked item in the browser
+
     let expanded_article = window.localStorage.getItem("expanded_article")
     let expanded_article_number = parseInt(expanded_article)
-    console.log(expanded_article_number)
+
+// pulling the default if the clicked-item is clicked again
     
     if (expanded_article_number == elementNumber){
-        document.querySelector('.js-grid').style.gridTemplateColumns="100px 100px 100px 100px"
+        document.querySelector('.js-grid').style.gridTemplateColumns="200px 200px 200px 200px"
         document.querySelector('.js-grid').style.gridTemplateRows="100px 100px 100px 100px"
         window.localStorage.clear()
         return
@@ -32,23 +41,25 @@
 
     window.localStorage.setItem("expanded_article", expanded_article_string) 
 
-        
+//Selecting the column of the clicked item
+
         switch(selectColumn) {
             case 0:
-                document.querySelector('.js-grid').style.gridTemplateColumns= "100px 100px 100px 500px";
+                document.querySelector('.js-grid').style.gridTemplateColumns= "200px 200px 200px 500px";
                 break;
             case 1:
-                document.querySelector('.js-grid').style.gridTemplateColumns= "500px 100px 100px 100px";
+                document.querySelector('.js-grid').style.gridTemplateColumns= "500px 200px 200px 200px";
                 break;
             case 2:
-                document.querySelector('.js-grid').style.gridTemplateColumns= "100px 500px 100px 100px";
+                document.querySelector('.js-grid').style.gridTemplateColumns= "200px 500px 200px 200px";
                 break;
             case 3:
-                document.querySelector('.js-grid').style.gridTemplateColumns= "100px 100px 500px 100px";
+                document.querySelector('.js-grid').style.gridTemplateColumns= "200px 200px 500px 200px";
                 break;
             default:
-                document.querySelector('.js-grid').style.gridTemplateColumns= "100px 100px 100px 100px";
+                document.querySelector('.js-grid').style.gridTemplateColumns= "200px 200px 200px 200px";
         }
+//Selecting the row of the clicked item
 
         switch(selectRow) {
             case 0:
